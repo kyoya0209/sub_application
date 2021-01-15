6d678d8a37a73619aacee0c54f164db916c49fca
Kyoya <k080209k@gmail.com>
2021-01-15 12:52:07 +0000

    amend mail setting for production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,16 +67,15 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: 'https://sub-application.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
-  host = 'sub-application.herokuapp.com'
-  config.action_mailer.default_url_options = { host: host }
-  ActionMailer::Base.smtp_settings = {
-    :port           => ENV['MAILGUN_SMTP_PORT'],
-    :address        => ENV['MAILGUN_SMTP_SERVER'],
-    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => host,
-    :authentication => :plain,
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    domain: 'gmail.com',
+    port: 587,
+    user_name: 'nikyunikyunikyu@gmail.com',
+    password: 'rswfwgcpcotfcjiv',
+    authentication: :login
   }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
